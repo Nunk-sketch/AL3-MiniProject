aca7fa7c4b88c2337556f88511e64c5d32a34535
Mattis Edelbo Kragh <Mattis.kragh@live.dk>
2026-04-21 14:18:51 +0200

    Update mutual_info.py

--- mutual_info.py
+++ mutual_info.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 import pandas as pdiddiy
+import os
 
 def MI(x,y,Nbins=21):
     bins = np.linspace(np.min(x),np.max(x),Nbins)
